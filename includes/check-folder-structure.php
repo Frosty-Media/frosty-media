74349e0cb6286bcf752ea5caff5d7715efcb0f9e
Austin Passy <austin@thefrosty.com>
2015-01-05 11:30:34 -0800

    folder rename function update.
    Deactivate current plugin after rename then reactivate.

--- includes/check-folder-structure.php
+++ includes/check-folder-structure.php
@@ -11,6 +11,19 @@ function frosty_media_check_folder_structure() {
 	if ( stristr( FM_DIRNAME, 'master' ) ) {
 		if ( rename( untrailingslashit( FM_PLUGIN_DIR ), str_replace( '-master', '', untrailingslashit( FM_PLUGIN_DIR ) ) ) ) {
 			add_option( 'frosty_media_GitHub_folder_renamed', true );
+			
+			// Lets be sure the activate & deactivate functions are present.
+			if ( !function_exists( 'activate_plugin' ) ) {
+				require( ABSPATH . 'wp-admin/includes/plugin.php' );
+			}
+			
+			// Lets deactivate the old plugin.	
+			deactivate_plugins( 'frosty-media-master/frosty-media.php' );
+			
+			// ...and activate the new one.
+			activate_plugin( 'frosty-media/frosty-media.php' );
+			
+			// Redirect to the dashboard.
 			wp_redirect( admin_url() );
 			exit;
 		}
