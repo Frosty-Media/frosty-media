26b4c5c39a96160fb25a778ebbea46c574ddbef0
Austin Passy <austin@passy.co>
2017-11-15 08:59:58 -0800

    Code cleanup.

--- views/list-table.php
+++ views/list-table.php
@@ -1,11 +1,11 @@
 <?php
 
-if ( ! class_exists( 'FrostyMedia\List_Table' ) ) {
-    require_once( trailingslashit( FM_PLUGIN_DIR ) . 'src/ListTable.php' );
+if ( ! class_exists( '\FrostyMedia\List_Table' ) ) {
+    require_once dirname( __DIR__ ) . '/src/list-table.php';
 }
 
 if ( ! isset( $this->title ) ) {
-    $notices_table = new FrostyMedia\Includes\ListTable();
+    $notices_table = new \FrostyMedia\Includes\ListTable();
     $notices_table->prepare_items( 4, true );
     $notices_table->display();
 } else { ?>
@@ -14,7 +14,7 @@ if ( ! isset( $this->title ) ) {
     frosty_media_screen_icon();
     printf( '<h2>Frosty Media %s</h2>', $this->title );
 
-    $notices_table = new FrostyMedia\Includes\ListTable();
+    $notices_table = new \FrostyMedia\Includes\ListTable();
     $notices_table->prepare_items();
     $notices_table->display(); ?>
     </div><?php
