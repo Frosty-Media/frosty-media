3aa518d61cf7c839d9256b4f1d7a875e12d9c38f
Austin Passy <austin@passy.co>
2017-11-15 08:59:31 -0800

    Cleanup and fix AJAX actions for activating licenses.

--- js/licenses.js
+++ js/licenses.js
@@ -1,3 +1,4 @@
+/** @global frosty_media_licenses */
 (function ($) {
   "use strict";
 
@@ -6,20 +7,23 @@
     var $this = null,
       $license,
       $plugin_id,
-      $plugin_action,
-      $text;
+      $plugin_action;
 
     var button,
       buttonwidth;
 
     // Variable input style width
-    $('form#' + frosty_media_licenses.dirname + '-licenses div.inside').each(function (index, element) {
-      $this = $(this);
+    $('form#' + frosty_media_licenses.dirname + '-licenses div.inside').each(function (i, el) {
+      $this = $(el);
       button = $this.children('.button-wrapper');
       buttonwidth = 100 * button.outerWidth() / $this.width();
       buttonwidth = buttonwidth <= 50 ? (98 - buttonwidth) : 100;
 
-      $this.children('input[type="text"]:first').css('width', buttonwidth + '%');
+      $this.find('input[name$="[license_key]"]').css({
+        width: buttonwidth + '%',
+        padding: '6px',
+        marginTop: '-1px'
+      });
     });
 
     // On click let's run the update.
@@ -32,30 +36,35 @@
       $license = $this.parents('div.inside').find('input[name="' + $plugin_id + '[license_key]"]');
       $plugin_action = $this.prop('name');
 
-      $this.before('<img id="img-' + $plugin_id + '" src="' + frosty_media_licenses.loading + '" style="margin:6.5px 0px 0px -30px;position:absolute;">');
+      $this.before('<img id="img-' + $plugin_id + '" src="' + frosty_media_licenses.loading + '" style="margin:6px 0 0 -30px;position:absolute;">');
 
       // Initiate a request to the server-side
-      $.post(ajaxurl, {
-        action: frosty_media_licenses.action,
-        nonce: frosty_media_licenses.nonce,
-        license: $license.val(),
-        plugin_id: $plugin_id,
-        plugin_action: $plugin_action,
-      }, function (response) {
+      var jqxhr = $.ajax({
+        method: "POST",
+        url: frosty_media_licenses.ajaxurl,
+        data: {
+          action: frosty_media_licenses.action,
+          nonce: frosty_media_licenses.nonce,
+          license: $license.val(),
+          plugin_id: $plugin_id,
+          plugin_action: $plugin_action,
+          plugin_nonce: $license.data('nonce')
+        },
+        success: function (response) {
+          if (typeof response.success !== 'undefined' && response.success) {
+            window.location.reload();
+          } else {
+            console.log(response);
+          }
+        },
+        fail: function () {
+          alert('Unknown Error');
+        }
+      });
 
-        if ('success' === response) {
-          location.reload();
-        }
-        else {
-          console.log(response);
-        }
-      })
-        .fail(function () {
-          alert("error");
-        })
-        .always(function () {
-          $('body').delay(1000).find('img#img-' + $plugin_id).remove();
-        });
+      jqxhr.always(function() {
+        $('body').delay(1000).find('img#img-' + $plugin_id).remove();
+      });
 
     });
 
