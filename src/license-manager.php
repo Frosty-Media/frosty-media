989ba8f67fa0d7b0234af7a2da679592210bfeb8
Austin Passy <austin@passy.co>
2017-11-28 07:45:11 -0800

    Remove unused const and add submenu_page property. Re-order properties.

--- src/license-manager.php
+++ src/license-manager.php
@@ -9,16 +9,17 @@ namespace FrostyMedia\Includes;
  */
 abstract class LicenseManager implements WpHooksInterface {
 
-    const OBJECT_NAME = null;
-
-    /** @var string $title */
-    protected $title;
+    /** @var string $submenu_page */
+    public $submenu_page;
 
     /** @var string $action */
     protected $action;
 
     /** @var string $api_url */
     protected $api_url;
+
+    /** @var string $title */
+    protected $title;
 
     /**
      * LicenseManager constructor.
