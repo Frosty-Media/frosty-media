c95b06cb175da909211cd427b53133fba16122fd
Austin Passy <austin@passy.co>
2017-11-28 07:08:46 -0800

    Add abstract base class and interface for classes.

---
+++ src/license-manager.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace FrostyMedia\Includes;
+
+abstract class LicenseManager implements WpHooksInterface {
+
+    const OBJECT_NAME = null;
+
+    /** @var string $title */
+    protected $title;
+
+    /** @var string $action */
+    protected $action;
+
+    /** @var string $api_url */
+    protected $api_url;
+
+    /**
+     * LicenseManager constructor.
+     */
+    public function __construct() {
+        $this->add_hooks();
+    }
+}
---
+++ src/wp-hooks-interface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace FrostyMedia\Includes;
+
+/**
+ * Interface WpHooksInterface
+ *
+ * Provides a contract for classes that add WordPress hooks
+ *
+ * @package FrostyMedia\Includes
+ */
+interface WpHooksInterface {
+
+    public function add_hooks();
+}
